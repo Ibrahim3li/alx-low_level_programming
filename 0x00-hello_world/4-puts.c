5c56968da60f4f058ee59945fb9742a862329469
ibrahim3li <ibrahim3li.9619@gmail.com>
2023-06-15 18:22:38 -0700

    doen done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - main entry point
+*returen : always 0 (success)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	returen(0);
+}
