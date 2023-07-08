23628ca2ef2c7d51df2d8ff7ab5186a01d24a9b1
ibrahim3li <ibrahim3li.9619@gmail.com>
2023-07-07 18:16:41 -0700

    edit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -21,14 +21,6 @@ int main(int argc __attribute__((unused)), char *argv[])
 	return (0);
 }
 
-/**
- * print_string - Prints all char of a string
- *
- * @str: Pointer to string
- *
- * Return: void
- */
-
 void print_string(char *str)
 {
 	int i = 0;
