d72566ac217209bb0217e947f2fc32923409c8cc
ibrahim3li <ibrahim3li.9619@gmail.com>
2023-07-28 15:17:09 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,31 +8,43 @@
  */
 int _strlen(char *s)
 {
-	int x = 0;
+    int i = 0;
 
-	if (!s)
-		return (0);
-	while (*s++)
-		x++;
-	return (x);
+    // Check if the string is NULL
+    if (!s)
+        return (0);
 
+    // Loop through the string until the null terminator '\0' is found
+    while (*s++)
+        i++;
+
+    // Return the length of the string
+    return (i);
 }
 
 /**
- * print_list - prints a linked lists
- * @h: pointer to first node
+ * print_list - prints a linked list
+ * @h: pointer to the first node
  *
- * Return: size of list
+ * Return: size of the list
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t y = 0;
+    size_t i = 0;
 
-	while (h)
-	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-		h = h->next;
-		y++;
-	}
-	return (y);
+    // Traverse the linked list while the current node is not NULL
+    while (h)
+    {
+        // Print the length of the string and the string content, or "(nil)" if NULL
+        printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+
+        // Move to the next node in the list
+        h = h->next;
+
+        // Increment the node counter
+        i++;
+    }
+
+    // Return the size of the list (number of nodes)
+    return (i);
+}
